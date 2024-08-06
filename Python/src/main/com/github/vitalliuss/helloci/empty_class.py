481962fed148584ed5671cd37bb766b49c9785e5
Vitali Shulha <vitalliuss@gmail.com>
2024-08-06 12:42:35 +0400

    Create Python/src/main/com/github/vitalliuss/helloci/empty_class.py

---
+++ Python/src/main/com/github/vitalliuss/helloci/empty_class.py
@@ -0,0 +1,13 @@
+# Empty class for Cobertura code coverage
+
+class EmptyClass:
+
+    @staticmethod
+    def empty_method():
+        # empty
+        pass
+
+    @staticmethod
+    def unused_method():
+        # do not use it
+        pass
